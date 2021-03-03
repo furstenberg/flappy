da946d2749ce984ef822edf8645add8c05893fbb
Michael Fürstenberg <michael@furstenberg.co>
2021-03-03 15:10:16 +0100

    Added second obstacle

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,8 @@ impl Obstacle {
 struct State {
     player: Player,
     frame_time: f32,
-    obstacle: Obstacle,
+    obstacle1: Obstacle,
+    obstacle2: Obstacle,
     mode: GameMode,
     score: i32,
 }
@@ -104,7 +105,8 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
-            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            obstacle1: Obstacle::new(SCREEN_WIDTH / 2, 0),
+            obstacle2: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
             score: 0,
         }
@@ -128,14 +130,23 @@ impl State {
         ctx.print(0, 0, "Press SPACE to flap.");
         ctx.print(0, 1, &format!("Score: {}", self.score));
 
-        self.obstacle.render(ctx, self.player.x);
+        self.obstacle1.render(ctx, self.player.x);
+        self.obstacle2.render(ctx, self.player.x);
 
-        if self.player.x > self.obstacle.x {
+        if self.player.x > self.obstacle1.x {
             self.score += 1;
-            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+            self.obstacle1 = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
         }
 
-        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
+        if self.player.x > self.obstacle2.x {
+            self.score += 1;
+            self.obstacle2 = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT
+            || self.obstacle1.hit_obstacle(&self.player)
+            || self.obstacle2.hit_obstacle(&self.player)
+        {
             self.mode = GameMode::End;
         }
     }
@@ -143,7 +154,8 @@ impl State {
     fn restart(&mut self) {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
-        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.obstacle1 = Obstacle::new(SCREEN_WIDTH / 2, 0);
+        self.obstacle2 = Obstacle::new(SCREEN_WIDTH, 0);
         self.mode = GameMode::Playing;
         self.score = 0;
     }
